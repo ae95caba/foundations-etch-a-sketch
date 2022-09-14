b55ba00c11e09f85021aa344b0be3f45b0e05d22
Andre Espinoza <ae95caba@gmail.com>
2022-09-14 07:02:20 -0300

    rewrite the comments for better understanding

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 const gridContainer = document.querySelector(".grid-container");
 
-let equisde ;
+let x ; // I defined this variable here because if I instead defined it inside the function below,
+// I wouldn 't be able to call it in the last line due to the scope
 
 function createDivs (num){
+    //creates divs inside div container
     for (let i = 1; i <= (num*num) ; i++ ){
         const createDiv = document.createElement("div");
         createDiv.classList.add("grid-div")
         gridContainer.appendChild(createDiv);
-        
+
     }
     
     const gridDivs = document.querySelectorAll(".grid-div");
-    console.log(gridDivs);
-    equisde=gridDivs;
-    
+    x=gridDivs;
+
+    // resizes each div of the grid and arranges them into rows and columns so all divs
+    //will fit in a 960px container
     gridDivs.forEach(a=>a.style.height =`\"${960/num}px\"`);
     gridDivs.forEach(a=>a.style.width =`\"${960/num}px\"`);
     gridContainer.style.gridTemplateRows=`repeat(${num},${100/num}%)`;
     gridContainer.style.gridTemplateColumns=`repeat(${num},${100/num}%)`;
 }
 
-createDivs(100);
-equisde.forEach(a => a.addEventListener("click",()=> a.style.backgroundColor = "red"));
+createDivs(64);
+x.forEach(a => a.addEventListener("mouseover",()=> a.style.backgroundColor = "red")); // last line
 
 
